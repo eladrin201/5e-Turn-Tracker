e278d3bca3cd00a638665d38d07b40adfe52c74c
Allison Betancourt <unreasonablyniceee@gmail.com>
2017-01-04 10:16:05 -0600

    Editing items in the list and state (#3)

--- src/js/components/ActorItem.jsx
+++ src/js/components/ActorItem.jsx
@@ -1,57 +1,101 @@
 import React, { PropTypes, Component } from 'react';
 import EditableInput from './EditableInput.jsx';
 
+let actor = {};
+
 export default class ActorItem extends Component {
   static propTypes = {
-    actorData: PropTypes.object.isRequired
+    actorData: PropTypes.object.isRequired,
+    updateItem: PropTypes.func.isRequired
   }
 
   render() {
     // Maping this.props to variable for ease
-    let actor = this.props.actorData;
+    actor = this.props.actorData;
+    console.log(actor);
 
     // Sections of form that will be conditionally toggled for the view
     let actorALL = (
       <div className="row">
         <div className="col-sm-2">
-          <EditableInput val={actor.initiative} label="Init" type="number"/>
+          <EditableInput val={actor.initiative}
+            label="Init"
+            id="initiative"
+            type="number"
+            update={this.props.updateItem} />
         </div>
         <div className="col-sm-3">
-          <EditableInput val={actor.name} label="Name" type="text"/>
+          <EditableInput val={actor.name}
+            label="Name"
+            id="name"
+            type="text"
+            update={this.props.updateItem} />
         </div>
         <div className="col-sm-2">
-          <EditableInput val={actor.health} label="Health" type="number"/>
+          <EditableInput val={actor.health}
+            label="Health"
+            id="health"
+            type="number"
+            update={this.props.updateItem} />
         </div>
         <div className="col-sm-2">
-          <EditableInput val={actor.armor} label="Armor" type="number"/>
+          <EditableInput val={actor.armor}
+            label="Armor"
+            id="armor"
+            type="number"
+            update={this.props.updateItem} />
         </div>
       </div>
     );
     let actorNPC = (
       <div className="row">
         <div className="col-sm-2">
-          <EditableInput val={actor.strength} label="STR" type="number"/>
+          <EditableInput val={actor.strength}
+            label="STR"
+            id="strength"
+            type="number"
+            update={this.props.updateItem} />
         </div>
         <div className="col-sm-2">
-          <EditableInput val={actor.dexterity} label="DEX" type="number"/>
+          <EditableInput val={actor.dexterity}
+            label="DEX"
+            id="dexterity"
+            type="number"
+            update={this.props.updateItem} />
         </div>
         <div className="col-sm-2">
-          <EditableInput val={actor.constitution} label="CON" type="number"/>
+          <EditableInput val={actor.constitution}
+            label="CON"
+            id="constitution"
+            type="number"
+            update={this.props.updateItem} />
         </div>
         <div className="col-sm-2">
-          <EditableInput val={actor.intelligence} label="INT" type="number"/>
+          <EditableInput val={actor.intelligence}
+            label="INT"
+            id="intelligence"
+            type="number"
+            update={this.props.updateItem} />
         </div>
         <div className="col-sm-2">
-          <EditableInput val={actor.wisdom} label="WIS" type="number"/>
+          <EditableInput val={actor.wisdom}
+            label="WIS"
+            id="wisdom"
+            type="number"
+            update={this.props.updateItem} />
         </div>
         <div className="col-sm-2">
-          <EditableInput val={actor.charisma} label="CHA" type="number"/>
+          <EditableInput val={actor.charisma}
+            label="CHA"
+            id="charisma"
+            type="number"
+            update={this.props.updateItem} />
         </div>
       </div>
     );
 
     // view return
-    if (this.props.actorType === 'npc') {
+    if (actor.actorType === 'npc') {
       return (
         <li>
           {actorALL}
